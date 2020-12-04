112748d4f831db78aea61fdf22553b6cd770d2f2
suschmitha007 <sushmitha.cs18@gmail.com>
2020-12-04 15:40:43 +0530

    creating and updating structs

---
+++ structs/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+type person struct {
+	firstName string
+	lastName  string
+	contact   contactInfo
+}
+
+func main() {
+	//Declaring Structs
+	// alex := person{firstName: "Alex", lastName: "Anderson"}
+
+	// other way.this assigns empty strings
+	// var alex person
+	// alex.firstName = "Alex"
+	// alex.lastName = "Anderson"
+	// fmt.Println(alex)
+	// fmt.Printf("%+v", alex)
+
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contact: contactInfo{
+			email:   "jim@gmail.com",
+			zipCode: 56738,
+		},
+	}
+	fmt.Printf("%+v", jim)
+}
