de5d38962695e17cd667f921dea7138d696dbe3f
suschmitha007 <sushmitha.cs18@gmail.com>
2020-12-05 18:47:31 +0530

    iterating over map

--- map/main.go
+++ map/main.go
@@ -4,21 +4,32 @@ import "fmt"
 
 func main() {
 	//Creating map with literal way
-	// colors := map[string]string{
-	// 	"red":   "#ff0000",
-	// 	"green": "#4bf745",
-	// }
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#4bf745",
+		"white": "#ffffff",
+	}
 
 	//2nd type
 	// var colors map[string]string
 
 	//3rd type
-	colors := make(map[string]string)
+	// colors := make(map[string]string)
 
-	colors["white"] = "ffffff"
-	fmt.Println(colors)
+	// updating map
+	// colors["white"] = "ffffff"
+	// fmt.Println(colors)
 
 	//Deleting key value pair
-	delete(colors, "white")
+	// delete(colors, "white")
+
 	fmt.Println(colors)
+	printMap(colors)
+}
+
+// iterating over maps
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("hex code for", color, "is", hex)
+	}
 }
