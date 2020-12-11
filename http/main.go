58969869d729688bd31a836261984100807e5356
suschmitha007 <sushmitha.cs18@gmail.com>
2020-12-11 16:49:11 +0530

    created reader and writer interface

--- http/main.go
+++ http/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
+type logWriter struct{}
 
 func main() {
 	resp, err := http.Get("http://google.com")
@@ -13,10 +15,24 @@ func main() {
 		os.Exit(1)
 	}
 	// To get the actual body read this document: https://golang.org/pkg/net/http/#Response
-	fmt.Println(resp)
-	bs := make([]byte, 99999)
-	resp.Body.Read(bs)
-	fmt.Println(string(bs))
+	// fmt.Println(resp)
+	// bs := make([]byte, 99999)
+	// resp.Body.Read(bs)
+	// fmt.Println(string(bs))
+
+	// Using Writer interface
+	// io.Copy(os.Stdout, resp.Body)
+
+	//Using custom Write
+	lw:=logWriter{}
+	io.Copy(lw,resp.Body)
 }
 
+// Custom Write Implementation
+func(logWriter) Write(bs []byte)(int,error){
+	fmt.Println(string(bs))
+	fmt.Println("just wrote these many bytes:",len(bs))
+	return len(bs), nil
+
+}
 // link for standard documenation  https://golang.org/pkg/net/http/, https://golang.org/pkg/net/http/#Get
